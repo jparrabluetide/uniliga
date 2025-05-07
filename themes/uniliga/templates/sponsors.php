f0401fc02552c52716671a6a31542c0042abcbcf
Juan Parra <jparra@bluetide.dev>
2025-05-07 11:09:57 -0500

    Maquetacion de los patrocinadores

---
+++ themes/uniliga/templates/sponsors.php
@@ -0,0 +1,20 @@
+<div class="container mx-auto px-4 my-8 md:my-24">
+  <h3 class="text-2xl md:text-4xl font-family-oswald uppercase text-tarawera-950 mb-7">Patrocinadores</h3>
+  <div class="grid grid-cols-5 gap-4">
+    <div class="col-span-5 sm:col-span-2 lg:col-span-1 min-h-[220px] md:min-h-[140px] bg-gray-300">
+      <img src="https://picsum.photos/id/237/300/230" class="w-full h-auto object-cover" alt="patrocinador" />
+    </div>
+    <div class="col-span-5 sm:col-span-2 lg:col-span-1 min-h-[220px] md:min-h-[140px] bg-gray-300">
+      <img src="https://picsum.photos/id/237/300/230" class="w-full h-auto object-cover" alt="patrocinador" />
+    </div>
+    <div class="col-span-5 sm:col-span-2 lg:col-span-1 min-h-[220px] md:min-h-[140px] bg-gray-300">
+      <img src="https://picsum.photos/id/237/300/230" class="w-full h-auto object-cover" alt="patrocinador" />
+    </div>
+    <div class="col-span-5 sm:col-span-2 lg:col-span-1 min-h-[220px] md:min-h-[140px] bg-gray-300">
+      <img src="https://picsum.photos/id/237/300/230" class="w-full h-auto object-cover" alt="patrocinador" />
+    </div>
+    <div class="col-span-5 sm:col-span-2 lg:col-span-1 min-h-[220px] md:min-h-[140px] bg-gray-300">
+      <img src="https://picsum.photos/id/237/300/230" class="w-full h-auto object-cover" alt="patrocinador" />
+    </div>
+  </div>
+</div>
