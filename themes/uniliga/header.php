5a573a22faad2b81b89f6c7352b28d24d6b73509
Juan Parra <jparra@bluetide.dev>
2025-06-04 12:40:22 -0500

    Crear clase general para cada sitio

--- themes/uniliga/header.php
+++ themes/uniliga/header.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-
+<?php 
+require_once('uniligaConfig.php');
+$uniliga = new Uniliga();
+?>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,21 +12,23 @@
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Roboto:wght@500&display=swap" rel="stylesheet">
   <script type="module" src="https://cdn.jsdelivr.net/npm/@justinribeiro/lite-youtube@1/lite-youtube.min.js"></script>
   <?php wp_head(); ?>
+  <?php
+  $blog_id = get_current_blog_id();
+  ?>
   <script>
     var siteConfig = {
       ajaxurl: '<?php echo admin_url('admin-ajax.php'); ?>',
       homeurl: '<?php echo esc_url(home_url('/')); ?>',
       nonce: '<?php echo wp_create_nonce('wp_rest'); ?>',
       lang: '<?php echo get_locale(); ?>',
+      blogId: '<?php echo $blog_id; ?>',
+      blogName: '<?php echo sanitize_title($uniliga->getSiteName()); ?>'
     }
   </script>
 </head>
 
-<?php
-$blog_id = get_current_blog_id();
-?>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(sanitize_title($uniliga->getSiteName())); ?> data-blogId="<?php echo $blog_id; ?>">
   <?php wp_body_open(); ?>
 
   <?php get_template_part('templates/mobileMainMenu', null); ?>
