09acd678f11509e6d5228d3682ccf34bd6c26995
Juan Parra <jparra@bluetide.dev>
2025-06-24 10:36:19 -0500

    Deshabilitar sponsors

--- plugins/uniliga/post-types/index.php
+++ plugins/uniliga/post-types/index.php
@@ -2,7 +2,7 @@
 
 function bluetide_post_types()
 {
-  require_once dirname(__FILE__) . '/sponsor.php';
+  //require_once dirname(__FILE__) . '/sponsor.php';
   require_once dirname(__FILE__) . '/news.php';
   require_once dirname(__FILE__) . '/gallery1.php';
   require_once dirname(__FILE__) . '/highlight.php';
