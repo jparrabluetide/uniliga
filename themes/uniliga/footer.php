e0bcb16a7adbeea19f83fecf22b883ad5d6e7810
Juan Parra <jparra@bluetide.dev>
2025-09-12 10:59:55 -0500

    Se crea el footer

--- themes/uniliga/footer.php
+++ themes/uniliga/footer.php
@@ -1,5 +1,7 @@
+<?php require_once WP_PLUGIN_DIR . '/uniliga/includes/bluetide-elementor-templates.php'; ?>
 <?php
-//echo Elementor\Plugin::instance()->frontend->get_builder_content_for_display(90);
+$template_id = get_id_by_slug('uniliga-footer', 'bluetide_template');
+echo Elementor\Plugin::instance()->frontend->get_builder_content_for_display($template_id);
 ?>
 
 <?php wp_footer(); ?>
